d3f0e8b696803877c79ba682301586a4db89e6e2
DimeryData <66977553+DimeryData@users.noreply.github.com>
2025-03-24 15:50:23 -0500

    Create Print_Function.py

---
+++ Print_Function.py
@@ -0,0 +1,19 @@
+#pip install usb --break-system-packages
+
+
+
+
+from escpos.printer import Usb
+import escpos
+# Update this with the output from USBDev.py
+vendor_id = 0x0485
+product_id = 0x5741
+printer = Usb(vendor_id, product_id)
+
+#printer.set(align='center', font='b', width=2, height=2)
+#printer._raw(b'\x1b\x21\x1e')
+printer._raw(b'\xb1\x21\x20')
+#printer._raw(b'\xb1\x40')
+printer.text("Hey There")
+
+printer.cut()
