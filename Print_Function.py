0140fddaf983adb009156a10cf910ba9f1d1b07f
DimeryData <66977553+DimeryData@users.noreply.github.com>
2025-03-31 16:41:18 -0500

    Update Print_Function.py
    Remove raw strings and simplify the code.

--- Print_Function.py
+++ Print_Function.py
@@ -1,20 +1,28 @@
-#pip install usb --break-system-packages
-
-
-
-
 from escpos.printer import Usb
 import escpos
 # Update this with the output from USBDev.py
+# Constants for the printer
+CONTROL_MODE = "Epson"
+BAUD_RATE = 9600
+DATA_BITS = 8
+STOP_BITS = 1
+PARITY = 'N'
 vendor_id = 0x0485
 product_id = 0x5741
+
 printer = Usb(vendor_id, product_id)
 
-#printer.set(align='center', font='b', width=2, height=2)
-#printer._raw(b'\x1b\x21\x1e')
-# Set alignment and font size (if needed)
-printer.set(align='center', font='b', width=2, height=2)
-#printer._raw(b'\xb1\x40')
-printer.text("Hey There")
+
+ALIGNMENT = 'center'
+FONT = 'b'
+WIDTH = 8
+HEIGHT = 8
+
+
+printer.set(align=ALIGNMENT, font=FONT, width=WIDTH, height=HEIGHT)
+
+
+printer.text("SUP DUDE?")
+
 
 printer.cut()
